da0ae30d46052b768905432fec0a368381eefaca
keihanikakauLOL <arm.nantana@gmail.com>
2026-02-10 16:40:18 +0700

    hard

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,24 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect R1, Rect R2){
+	double xleft = max(R1.x, R2.x);
+	double xright = min(R1.x + R1.w, R2.x + R2.w);
+	double ytop = min(R1.y, R2.y);
+	double ybottom = max(R1.y - R1.h, R2.y - R2.h);
 
+	double Wover = xright - xleft;
+	double Lover = ytop - ybottom;
 
-
+	if(Wover > 0 && Lover > 0) {
+		return Wover * Lover;
+	}
+	else {
+		return 0;	
+	}
 }
